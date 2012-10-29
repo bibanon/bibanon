532894098e7306dac0bdd62f826a02c952126289
Antonizoon <antonizoon@bibanon.org>
2012-10-29 00:59:40 -0700

    Phase 2 of Damaged Goods Remastering Project begins

--- Bibanon/tools/media2iki/wiki2md.sh
+++ Bibanon/tools/media2iki/wiki2md.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# This script do the following:
+# This script does the following to all .mediawiki files in the current directory:
 # 1. uses the mediawiki2html script to convert the mediawiki to HTML.
 # 2. uses pandoc to convert HTML to markdown (leaving any unknown HTML as raw)
 # 3. htmltable2pandoc to convert HTML tables into markdown syntax.
+# 4. sed to restore wikilinks
 
 find -type f -name \*.mediawiki | while read i; do
 NAME=`echo $i | sed -e's/.mediawiki$//'`
