c09339b40c449eca3fb68246e38e7fa7e6c2e1d9
Tenshi Hinanawi <cockmomgler@gmail.com>
2013-03-14 01:21:53 -0700

    added BA-4chan-Archiver (now it's actually somewhat useful!)

--- Bibanon/tools/BA-4chan-Archiver/grab-thread.sh
+++ Bibanon/tools/BA-4chan-Archiver/grab-thread.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-$tnumbertnumber=`echo $1 | cut -c32-`
+boardid="$1"
+tnumber="$2"
 
-mkdir $tnumber
-cd $tnumber
+echo "$boardid $tnumber"
 
+echo "Starting thread text archival..."
 while :
 	do
-		wget -e robots=off -E -nd -nc -np -r -k -H -D images.4chan.org,thumbs.4chan.org $1
-		cp $tnumber.html index.html
+		curl http://api.4chan.org/$boardid/res/$tnumber.json | python2 -mjson.tool > b-$tnumber.json
 		sleep 10
 	done
 #backup
